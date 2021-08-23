5475a7758b972e124a403f792993e913daa23bf0
raiyan <raiyan5764@gmail.com>
2021-08-23 13:39:00 +0600

    carousel slider

--- carousel/app.js
+++ carousel/app.js
@@ -1,1 +1,15 @@
-const images = document.querySelectorAll("img");
+const images = document.querySelector(".imgs");
+
+const img = document.querySelectorAll(".imgs img");
+console.log(img);
+
+let i = 0;
+
+function run() {
+  i++;
+  if (i > img.length - 1) {
+    i = 0;
+  }
+  images.style.transform = `translateX(${-i * 1000}px)`;
+}
+setInterval(run, 4000);
