9c1f8d6f36db3efac080d309ba84b9d9b5d5aa8b
raiyan <raiyan5764@gmail.com>
2021-08-25 21:44:54 +0600

    zoom image add

---
+++ zoom-picture/app.js
@@ -0,0 +1,18 @@
+const track = document.querySelector(".track");
+const myFakeImg = document.querySelector(".left-side-img img");
+const mainImg = document.querySelector(".main-image");
+myFakeImg.addEventListener("mousemove", function (e) {
+  let x = e.clientX - e.target.offsetLeft;
+  let y = e.clientY - e.target.offsetTop;
+
+  myFakeImg.style.cursor = "zoom-in";
+  mainImg.style.visibility = "visible";
+  console.log(x, y);
+  mainImg.style.transformOrigin = `${x}px ${y}px `;
+  mainImg.style.transform = "scale(2)";
+});
+
+myFakeImg.addEventListener("mouseout", function () {
+  mainImg.style.visibility = "hidden";
+  track.style.display = "none";
+});
